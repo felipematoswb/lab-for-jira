b61659200f29126b95ef2d890e4efbcd617ab2de
Felipe Matos <felipematoswb@gmail.com>
2021-01-26 15:22:51 -0300

    Stack ELK - Ok | Postgres Ok

--- postgres-container/init-user-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER user_jira;
-    ALTER USER user_jira WITH PASSWORD 'pass_jira' -d template1
-    CREATE DATABASE jiradb WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;
-    GRANT ALL PRIVILEGES ON DATABASE jiradb TO user_jira;
-EOSQL
